d132fc11a850800d0e0ae03b0846bc9e2edd5a96
Gerardo Perez <gerardo.perez@usach.cl>
2024-12-19 20:22:28 -0300

    board-can-play

--- board_21343296_GerardoPerezEncina.rkt
+++ board_21343296_GerardoPerezEncina.rkt
@@ -1,7 +1,7 @@
 #lang racket
 (require "piece_21343296_GerardoPerezEncina.rkt")
 (provide board)
-;(provide board-can-play?)
+(provide board-can-play?)
 ;(provide board-set-play-piece)
 ;(provide board-check-vertical-win)
 ;(provide board-check-horizontal-win)
@@ -31,3 +31,20 @@
       '()
       (cons (crear-fila columnas) (board (- filas 1) columnas)))
   )
+
+; ------------------------------------------------
+; Nombre: board-can-play?
+; Descripcion: Verifica si es que se puede jugar en el tablero, si hay al menos un espacio vacio, devuelve #t y en caso contrario #f
+; Dominio: board (lista de listas)
+; Recorrido: booleano (#t o #f)
+
+(define (board-can-play? board)
+  (define (fila-tiene-espacio? fila)
+    (member 'empty fila)) 
+  (define (revisar-tablero filas)
+    (if (null? filas)
+        #f  
+        (if (fila-tiene-espacio? (car filas))
+            #t 
+            (revisar-tablero (cdr filas)))))  
+  (revisar-tablero board))
