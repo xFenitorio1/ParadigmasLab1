80a8542de4ab39aab2a3478fd0b7122ef32125f8
Gerardo Perez <gerardo.perez@usach.cl>
2024-12-23 17:44:10 -0300

    game-is-draw?

--- game_21343296_GerardoPerezEncina.rkt
+++ game_21343296_GerardoPerezEncina.rkt
@@ -3,7 +3,7 @@
 (require "piece_21343296_GerardoPerezEncina.rkt");importa la pieza
 (require "board_21343296_GerardoPerezEncina.rkt")
 (provide game)
-;(provide game-is-draw?)
+(provide game-is-draw?)
 ;(provide game-get-current-player)
 ;(provide game-get-board)
 ;(provide game-set-end)
@@ -20,3 +20,33 @@
 
 (define (game player1 player2 board current-turn)
   (list player1 player2 board current-turn '())) ;El ultimo es para el historial
+
+; ------------------------------------------------
+; Nombre: game-history
+; Descripcion: 
+; Dominio: player1 (player) 
+; Recorrido:
+
+; ------------------------------------------------
+; Nombre: game-is-draw?
+; Descripcion: Verifica si el juego actual es un empate
+; Dominio: game (estructura del juego)
+; Recorrido: booleano (#t si es empate, #f si no)
+
+(define (game-is-draw? game)
+
+  (define tablero (car (cddr game))) 
+  (define jugador1 (car game))       
+  (define jugador2 (cadr game))       
+
+
+  (define tablero-lleno (not (board-can-play? tablero)))
+
+
+  (define jugadores-sin-fichas
+    (and (= (player-remaining-pieces jugador1) 0)
+         (= (player-remaining-pieces jugador2) 0)))
+
+  (or tablero-lleno jugadores-sin-fichas))
+
+; ------------------------------------------------
