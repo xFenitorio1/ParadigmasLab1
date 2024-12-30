2d2a7eb397e0c251b5012659eff61083eb05e13b
Gerardo Pérez <gerardo.perez@usach.cl>
2024-12-30 02:50:51 -0300

    Delete TDAPiece.rkt (IGNORAR)

--- TDAPiece.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket
-(provide piece-color)
-; ------------------------------------------------
-; Nombre: piece-color
-; Descripcion: Obtiene el color (string) de una piece
-; Dominio: piece
-; Recorrido: color(string)
-(define (piece-color piece) (car piece))
